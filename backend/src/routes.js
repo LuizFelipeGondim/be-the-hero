ef187124c1a4654512f6b84213e2102b495b070f
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-03-25 17:28:47 -0300

    Backend da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const CasoController = require('./controllers/CasoController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController.js.js');
+
+
+const routes = express.Router();
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/casos', CasoController.index);
+routes.post('/casos', CasoController.create);
+
+routes.delete('/casos/:id', CasoController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/sessions', SessionController.create);
+
+module.exports = routes;
